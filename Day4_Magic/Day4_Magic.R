4f1bdbfb260b5f2ea929dff7a826333261313d52
Tim Schoof <timschoof@hotmail.com>
2021-04-03 17:51:39 -0500

    readme

---
+++ Day4_Magic/Day4_Magic.R
@@ -0,0 +1,12 @@
+# Day 4
+
+# https://github.com/bradleyboehmke/harrypotter
+
+# install packages
+library(harrypotter)
+
+# Each text is in a character vector with each element representing a single chapter. 
+d <- harrypotter::chamber_of_secrets # the whole book
+
+
+# plot most frequent words per book
